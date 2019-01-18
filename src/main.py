27ae232ce9fa5e28e6f19e677ac69df1a91df9ae
ruwix <grantpauker@gmail.com>
2019-01-17 19:16:35 -0800

    Better thresholding for boxes,  draw contours, add threshold params

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from client import Client
 from vision import Vision
+import cv2
 client = Client("10.29.84.2")
-vision = Vision()
+vision = Vision(filename="images/photo2.jpg")
 vision.update()
 
-while True:
-    vision.update()
-    movement = vision.getMovement()
-    client.table.putNumberArray(movement)
+cv2.imshow("Image", vision.frame)
+cv2.waitKey(0)
+
+# while True:
+#     vision.update()
+#     movement = vision.getMovement()
+#     client.table.putNumberArray(movement)
--- src/vision.py
+++ src/vision.py
@@ -5,9 +5,14 @@ import boundingbox
 
 
 class Vision:
-    def __init__(self, filename=None):
-        self.camera = cv2.VideoCapture(0)
+    def __init__(self, lower_thresh=180, upper_thresh=255, x_thresh=0.05, filename=None):
+        self.camera = cv2.VideoCapture()
+        if not self.camera.isOpened():
+            print("WARNING: No camera found")
         self.filename = filename
+        self.lower_thresh = lower_thresh
+        self.upper_thresh = upper_thresh
+        self.x_thresh = x_thresh
         self.thresh = None
         self.contours = None
         self.boxes = []
@@ -19,12 +24,14 @@ class Vision:
             _, self.frame = self.camera.read()
         else:
             self.frame = cv2.imread(self.filename)
+        imgray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+        hsv = cv2.split(imgray)
+
         bgr = cv2.split(self.frame)
         green = cv2.bitwise_xor(bgr[2], bgr[1])
-
-        lower_thresh = 190
-        upper_thresh = 255
-        _, self.thresh = cv2.threshold(green, lower_thresh, upper_thresh, 0)
+        green = cv2.bitwise_and(green, hsv[2])
+        _, self.thresh = cv2.threshold(
+            green, self.lower_thresh, self.upper_thresh, 0)
 
     def updateContours(self):
         self.contours, _ = cv2.findContours(self.thresh, cv2.RETR_TREE,
@@ -42,12 +49,11 @@ class Vision:
                 else:
                     center = (0, 0)
                 offset = (center[0]/width, center[1]/height)
-                # output = cv2.drawContours(self.frame, [cnt], -1, (0, 255, 0), 2)
-                # output = cv2.circle(self.frame, center, 5, (0, 0, 255), -1)
-                # cv2.imshow("Image", self.frame)
+                output = cv2.drawContours(
+                    self.frame, [cnt], -1, (255, 0, 0), 2)
+                output = cv2.circle(self.frame, center, 5, (0, 0, 255), -1)
                 self.boxes.append(boundingbox.BoundingBox(
                     cnt, center, area, offset))
-        # cv2.waitKey(0)
 
     def updateMovement(self):
         length = len(self.boxes)
@@ -56,9 +62,8 @@ class Vision:
 
         x = (self.boxes[1].offset[0] + self.boxes[0].offset[0])/2
         y = (self.boxes[1].offset[1] + self.boxes[0].offset[1])/2
-        x_thresh = 0.05
         x_error = x - 0.5
-        if abs(x_error) > x_thresh:
+        if abs(x_error) > self.x_thresh:
             self.movement[0] = x_error * 2
 
     def update(self):
